ebb7d5535f1fdbdc66a26fb1edc0baf5d3ab0e2d
Fabio Perez <fabiovmp@gmail.com>
2015-05-31 21:38:04 -0300

    Add parameters

--- src/detect.cpp
+++ src/detect.cpp
@@ -1,3 +1,7 @@
+/* HOG DETECTOR
+ *
+ */
+
 #include <dlib/svm_threaded.h>
 #include <dlib/gui_widgets.h>
 #include <dlib/image_processing.h>
@@ -15,25 +19,52 @@ using namespace dlib;
 int main(int argc, char** argv) {
   try {
     command_line_parser parser;
-    parser.parse(argc, argv);
 
-    const std::string dir = argv[1];
+    parser.add_option("h","Display this help message.");
+    parser.add_option("u", "Upsample each input image <arg> times. Each upsampling quadruples the number of pixels in the image (default: 0).", 1);
+
+    parser.parse(argc, argv);
+    parser.check_option_arg_range("u", 0, 8);
+
+    const char* one_time_opts[] = {"h","u"};
+    parser.check_one_time_options(one_time_opts);
+
+    // Display help message
+    if (parser.option("h")) {
+      cout << "Usage: " << argv[0] << " [options] <list of images>" << endl;
+      parser.print_options(); 
+
+      return EXIT_SUCCESS;
+    }
+
+    if (parser.number_of_arguments() == 0) {
+      cout << "You must give a list of input files." << endl;
+      cout << "\nTry the -h option for more information." << endl;
+      return EXIT_FAILURE;
+    }
+
+    const unsigned long upsample_amount = get_option(parser, "u", 0);
 
     dlib::array<array2d<unsigned char> > images;
 
     images.resize(parser.number_of_arguments());
     for (unsigned long i = 0; i < images.size(); ++i) load_image(images[i], parser[i]);
-    for (unsigned long j = 0; j < images.size(); ++j) pyramid_up(images[j]);
-    //for (unsigned long j = 0; j < images.size(); ++j) pyramid_up(images[j]);
+    for (unsigned long i = 0; i < upsample_amount; ++i) {
+      for (unsigned long j = 0; j < images.size(); ++j) {
+        pyramid_up(images[j]);
+      }
+    }
     typedef scan_fhog_pyramid<pyramid_down<6> > image_scanner_type; 
 
     // Load SVM detectors
-    object_detector<image_scanner_type> detector1, detector2;
+    object_detector<image_scanner_type> detector1, detector2, detector3;
     deserialize("svm_detectors/pare_detector.svm") >> detector1;
     deserialize("svm_detectors/lombada_detector.svm") >> detector2;
+    deserialize("svm_detectors/pedestre_detector.svm") >> detector3;
     std::vector<object_detector<image_scanner_type> > detectors;
     detectors.push_back(detector1);
     detectors.push_back(detector2);
+    detectors.push_back(detector3);
 
     image_window win;
     std::vector<rect_detection> rects;
@@ -43,9 +74,26 @@ int main(int argc, char** argv) {
       // Put the image and detections into the window.
       win.clear_overlay();
       win.set_image(images[i]);
+      std::string text_desc;
+
       for (unsigned long j = 0; j < rects.size(); ++j) {
-        win.add_overlay(rects[j].rect, rgb_pixel(255,rects[j].weight_index*255,0));
+        switch (rects[j].weight_index) {
+          case 0: 
+            text_desc = "PARE";
+            break;
+          case 1: 
+            text_desc = "LOMBADA";
+            break;
+          case 2: 
+            text_desc = "PEDESTRE";
+            break;
+          default: break;
+        }
+
+        win.add_overlay(rects[j].rect, rgb_pixel(255,rects[j].weight_index*122,0),text_desc);
       }
+
+      //cin.get();
 
     }
 
