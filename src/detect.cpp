46d38694250b05255f2cda1ecb737d58a7a9296c
OrBaruk <orcansian@gmail.com>
2015-06-19 15:14:15 -0300

    Use correlation_tracker

--- src/detect.cpp
+++ src/detect.cpp
@@ -23,11 +23,13 @@ int main(int argc, char** argv) {
     parser.add_option("h","Display this help message.");
     parser.add_option("u", "Upsample each input image <arg> times. Each upsampling quadruples the number of pixels in the image (default: 0).", 1);
     parser.add_option("wait","Wait user input to show next image.");
+    parser.add_option("t","Uses a correlation tracker to improve performance at cost of precision. Signs locations are reevaluated every <arg> frames.", 1);
 
     parser.parse(argc, argv);
     parser.check_option_arg_range("u", 0, 8);
+    parser.check_option_arg_range("t", 0, 999);
 
-    const char* one_time_opts[] = {"h","u","wait"};
+    const char* one_time_opts[] = {"h","u","wait", "t"};
     parser.check_one_time_options(one_time_opts);
 
     // Display help message
@@ -67,40 +69,94 @@ int main(int argc, char** argv) {
     detectors.push_back(detector2);
     detectors.push_back(detector3);
 
+
     image_window win;
     std::vector<rect_detection> rects;
-    for (unsigned long i = 0; i < images.size(); ++i) {
-      evaluate_detectors(detectors, images[i], rects);
+    const int MAX_ITERATIONS = get_option(parser, "t", 0); 
+    if (MAX_ITERATIONS == 0) {
+      for (unsigned long i = 0; i < images.size(); ++i) {
+        evaluate_detectors(detectors, images[i], rects);
 
-      // Put the image and detections into the window.
-      win.clear_overlay();
-      win.set_image(images[i]);
-      std::string text_desc;
+        // Put the image and detections into the window.
+        win.clear_overlay();
+        win.set_image(images[i]);
+        std::string text_desc;
 
-      for (unsigned long j = 0; j < rects.size(); ++j) {
-        switch (rects[j].weight_index) {
-          case 0: 
-            text_desc = "PARE";
-            break;
-          case 1: 
-            text_desc = "LOMBADA";
-            break;
-          case 2: 
-            text_desc = "PEDESTRE";
-            break;
-          default: break;
+        for (unsigned long j = 0; j < rects.size(); ++j) {
+          switch (rects[j].weight_index) {
+            case 0: 
+              text_desc = "PARE";
+              break;
+            case 1: 
+              text_desc = "LOMBADA";
+              break;
+            case 2: 
+              text_desc = "PEDESTRE";
+              break;
+            default: 
+              break;
+          }
+          win.add_overlay(rects[j].rect, rgb_pixel(255,rects[j].weight_index*122,0),text_desc);
         }
 
-        win.add_overlay(rects[j].rect, rgb_pixel(255,rects[j].weight_index*122,0),text_desc);
+        if (parser.option("wait")) {
+          cout << "Press any key to continue...";
+          cin.get();
+        }
       }
-
-    if (parser.option("wait")) {
-      cout << "Press any key to continue...";
-      cin.get();
     }
+    else {
+      const int MAX_TRACKERS = 100;
+      correlation_tracker tracker[MAX_TRACKERS];
+      int tracker_label[MAX_TRACKERS];
+      std::string tracker_text[MAX_TRACKERS];
 
+
+      for (unsigned long i = 0; i < images.size();) { 
+        evaluate_detectors(detectors, images[i], rects);      
+        for (unsigned long j = 0; j < rects.size() && j < MAX_TRACKERS; ++j) {
+          tracker[j].start_track(images[i], rects[j].rect);
+          tracker_label[j] = rects[j].weight_index;
+        }
+        
+        for (int k = 0; k < MAX_ITERATIONS && i < images.size(); ++k) {
+          for (int j = 0; j < rects.size() && j < MAX_TRACKERS; ++j) {      
+            // Update tracker
+            tracker[j].update(images[i]);
+
+            // Get sign name
+            switch (tracker_label[j]) {
+              case 0: 
+                tracker_text[j] = "PARE";
+                break;
+              case 1: 
+                tracker_text[j] = "LOMBADA";
+                break;
+              case 2: 
+                tracker_text[j] = "PEDESTRE";
+                break;
+              default: 
+                break;
+            }
+          }
+          
+          win.clear_overlay();
+          win.set_image(images[i]);
+
+          for (int j = 0; j < rects.size() && j < MAX_TRACKERS; ++j) {
+            win.add_overlay(tracker[j].get_position(), rgb_pixel(255,rects[j].weight_index*122,0), tracker_text[j]);
+          }
+
+          // Wait for user input
+          if (parser.option("wait")) {
+            cout << "Press any key to continue...";
+            cin.get();
+          }
+
+          i++;
+        }
+      }
     }
-
   }
   catch (exception& e) {
     cout << "\nexception thrown!" << endl;
